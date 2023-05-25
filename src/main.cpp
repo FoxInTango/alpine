9c8f904ad388e7d5ef1b9fbaf7a689d72b76b49a
Jack.WSL <foxintango@yeah.net>
2023-05-25 16:44:24 +0800

    alpine

--- src/main.cpp
+++ src/main.cpp
@@ -74,19 +74,11 @@ int read_file(){
 }
 int write_file(){
     FILE* fp = NULL;
-    char buff[255];
 
-    fp = fopen("/tmp/test.txt", "r");
-    fscanf(fp, "%s", buff);
-    printf("1: %s\n", buff);
-
-    fgets(buff, 255, (FILE*)fp);
-    printf("2: %s\n", buff);
-
-    fgets(buff, 255, (FILE*)fp);
-    printf("3: %s\n", buff);
+    fp = fopen("/tmp/test.txt", "w+");
+    fprintf(fp, "This is testing for fprintf...\n");
+    fputs("This is testing for fputs...\n", fp);
     fclose(fp);
-
     return 0;
 }
 
