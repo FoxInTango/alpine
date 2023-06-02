631a60faea81af934877ad42207b2bfacd3f1466
Jack.WSL <foxintango@yeah.net>
2023-06-03 00:44:32 +0800

    alpine

--- src/main.cpp
+++ src/main.cpp
@@ -73,7 +73,7 @@ int main(int argc, char* argv[]) {
     //int_array.insert(0,0); 
     String s("这是一个悲伤的故事");
     std::cout << "string length " << s.length() << std::endl;
-    std::wcout << (wchar_t)(s.unicode()[0]) << std::endl;
+    std::wcout << "这是一个悲伤的故事" << (wchar_t)(s.unicode()[0]) << std::endl;
     char* n = 0;
     s.as(&n);
     std::cout << "utf8 : " << n << std::endl;
