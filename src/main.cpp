383043ebaaa9cfffb6d26359f97abf512ca39184
Jack.WSL <foxintango@yeah.net>
2023-06-03 00:56:04 +0800

    alpine

--- src/main.cpp
+++ src/main.cpp
@@ -79,9 +79,7 @@ int main(int argc, char* argv[]) {
     char* n = 0;
     Size l = s.as(&n);
     std::cout << "utf8 : " << n << std::endl;
-    if(n == 0) {
-        std::cout << "utf8 转换失败 : L == " << l  << std::endl;
-    }
+    std::cout << "utf8 转换失败 : L == " << l << std::endl;
 
     int endian = endianType();
     if(ENDIAN_B == endian) std::cout << "endian type : ENDIAN_B" << std::endl;
