7f17a3967d8434aebb2524137034b7fb9f7c9f51
Jack.WSL <foxintango@yeah.net>
2023-06-03 17:17:09 +0800

    alpine

--- src/main.cpp
+++ src/main.cpp
@@ -87,7 +87,7 @@ int main(int argc, char* argv[]) {
     std::cout << "alpine.onevent :" << alpine.onevent(alpine, 10) << std::endl;
 #endif
     */
-    char* utf = "这是一个悲伤的故事";
+    char* utf = "It's a sad story.";
     unsigned char p = 255;
     prefix((unsigned char*)utf,p);
     
