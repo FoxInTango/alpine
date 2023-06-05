d555c204df6c1caef074efcb0b09f2a2ab297316
Jack.WSL <foxintango@yeah.net>
2023-06-05 13:37:59 +0800

    alpine

--- src/main.cpp
+++ src/main.cpp
@@ -56,7 +56,7 @@ void echo_utf8_bits(unsigned char* utf8){
     Index index32 = 0;
     char charactor[8];
     while (utf8[index8]) {
-        memclr(charactor,sizeof(charactor),0);
+        memclr(charactor,(Size)sizeof(charactor),0);
         if (utf8[index8] < 0b10000000) {
             charactor[0] = utf8[index8];
             std::cout << "charactor : " << charactor << "  bits : " << std::bitset<8>(charactor[0]) << std::endl;
