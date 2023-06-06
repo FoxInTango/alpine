5beea65241a272f6c05d8dff3278eb5e10b68cf0
Jack.WSL <foxintango@yeah.net>
2023-06-06 22:56:18 +0800

    alpine

--- src/main.cpp
+++ src/main.cpp
@@ -269,13 +269,13 @@ Error error;
      printf("\033[0;30;47m color!!! \033[0m Hello \n");
 #include <fstream>
      std::ifstream input_stream("input");
-     std::cin.rdbuf(input_stream.rdbuf);
+     std::streambuf* cin_stream = std::cin.rdbuf(input_stream.rdbuf());
      char input[128];
      memclr(input,128,0);
      while(std::cin >> input){
          std::cout << "输入内容: " << input << std::endl;
          if(input[0] == 'q') {
-         std::cin.rdbuf(std::cin);
+         std::cin.rdbuf(cin_stream);
          input_stream.close();
 
          break;
