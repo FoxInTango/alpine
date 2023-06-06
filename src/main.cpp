977abdcca804535cd3ef15f2f17d6b45c59111c8
Jack.WSL <foxintango@yeah.net>
2023-06-06 21:37:18 +0800

    alpine

--- src/main.cpp
+++ src/main.cpp
@@ -238,6 +238,35 @@ Error error;
 
      std::cout << "Array size " << array.size() * sizeof(unsigned int) << std::endl;
      */
+
+     printf("************************* \n");
+     printf("\033[0;30;41m color!!! \033[0m Hello \n");
+     printf("\033[1;30;41m color!!! \033[0m Hello \n");
+     printf("\033[4;30;41m color!!! \033[0m Hello \n");
+     printf("\033[5;30;41m color!!! \033[0m Hello \n");
+     printf("\033[7;30;41m color!!! \033[0m Hello \n");
+     printf("\033[8;30;41m color!!! \033[0m Hello \n");
+
+
+     printf("************************* \n");
+     printf("\033[0;30;41m color!!! \033[0m Hello \n");
+     printf("\033[0;31;41m color!!! \033[0m Hello \n");
+     printf("\033[0;32;41m color!!! \033[0m Hello \n");
+     printf("\033[0;33;41m color!!! \033[0m Hello \n");
+     printf("\033[0;34;41m color!!! \033[0m Hello \n");
+     printf("\033[0;35;41m color!!! \033[0m Hello \n");
+     printf("\033[0;36;41m color!!! \033[0m Hello \n");
+     printf("\033[0;37;41m color!!! \033[0m Hello \n");
+
+     printf("************************* \n");
+     printf("\033[0;30;40m color!!! \033[0m Hello \n");
+     printf("\033[0;30;41m color!!! \033[0m Hello \n");
+     printf("\033[0;30;42m color!!! \033[0m Hello \n");
+     printf("\033[0;30;43m color!!! \033[0m Hello \n");
+     printf("\033[0;30;44m color!!! \033[0m Hello \n");
+     printf("\033[0;30;45m color!!! \033[0m Hello \n");
+     printf("\033[0;30;46m color!!! \033[0m Hello \n");
+     printf("\033[0;30;47m color!!! \033[0m Hello \n");
     return 0;
 }
 
