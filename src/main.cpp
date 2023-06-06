e2c354fe626fa811f97b7a98caf99897253c646d
Jack.WSL <foxintango@yeah.net>
2023-06-06 23:03:14 +0800

    alpine

--- src/main.cpp
+++ src/main.cpp
@@ -272,7 +272,7 @@ Error error;
      std::streambuf* cin_stream = std::cin.rdbuf(input_stream.rdbuf());
      char input[128];
      memclr(input,128,0);
-     while(true){
+     while(std::cin.get()){
          std::cin >> input;
          std::cout << "输入内容: " << input << std::endl;
          //if(input[0] == 'q') {
