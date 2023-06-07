054a11aa0685ea3443e5c2c8b0ab6ce7e0b929d0
Jack.WSL <foxintango@yeah.net>
2023-06-07 15:28:52 +0800

    alpine

--- src/main.cpp
+++ src/main.cpp
@@ -217,18 +217,20 @@ Error error;
 
     /** Array 插入 测试
      */
-     for(unsigned int i = 0;i < 40960;i ++){
+     for(unsigned int i = 0;i < 1024;i ++){
          array.append(i);
      }
 
-     for(unsigned int i = 0;i < 4096;i ++){
-         array.insert(20230603,i * 10);
-     }
+     Array<unsigned int> new_array(array);
      /*
-     for (int i = 0; i < array.size(); i++) {
+     for(unsigned int i = 0;i < 1024;i ++){
+         array.insert(20230603,i * 10);
+     }*/
+     
+     for (int i = 0; i < new_array.size(); i++) {
          std::cout << "Array Element " << i << " : " << array[i] << std::endl;
      }
-     */
+     
      /*
      for(unsigned int i = 0;i < 4096;i ++){
          if(array[i * 10] == 20230603){
@@ -238,38 +240,10 @@ Error error;
 
      std::cout << "Array size " << array.size() * sizeof(unsigned int) << std::endl;
      */
-
-     printf("************************* \n");
-     printf("\033[0;30;41m color!!! \033[0m Hello \n");
-     printf("\033[1;30;41m color!!! \033[0m Hello \n");
-     printf("\033[4;30;41m color!!! \033[0m Hello \n");
-     printf("\033[5;30;41m color!!! \033[0m Hello \n");
-     printf("\033[7;30;41m color!!! \033[0m Hello \n");
-     printf("\033[8;30;41m color!!! \033[0m Hello \n");
-
-
-     printf("************************* \n");
-     printf("\033[0;30;41m color!!! \033[0m Hello \n");
-     printf("\033[0;31;41m color!!! \033[0m Hello \n");
-     printf("\033[0;32;41m color!!! \033[0m Hello \n");
-     printf("\033[0;33;41m color!!! \033[0m Hello \n");
-     printf("\033[0;34;41m color!!! \033[0m Hello \n");
-     printf("\033[0;35;41m color!!! \033[0m Hello \n");
-     printf("\033[0;36;41m color!!! \033[0m Hello \n");
-     printf("\033[0;37;41m color!!! \033[0m Hello \n");
-
-     printf("************************* \n");
-     printf("\033[0;30;40m color!!! \033[0m Hello \n");
-     printf("\033[0;30;41m color!!! \033[0m Hello \n");
-     printf("\033[0;30;42m color!!! \033[0m Hello \n");
-     printf("\033[0;30;43m color!!! \033[0m Hello \n");
-     printf("\033[0;30;44m color!!! \033[0m Hello \n");
-     printf("\033[0;30;45m color!!! \033[0m Hello \n");
-     printf("\033[0;30;46m color!!! \033[0m Hello \n");
-     printf("\033[0;30;47m color!!! \033[0m Hello \n");
+/*
 #include <fstream>
-     std::ifstream input_stream("./input.txt");
-     std::streambuf* cin_stream = std::cin.rdbuf(input_stream.rdbuf());
+     //std::ifstream input_stream("./input.txt");
+     //std::streambuf* cin_stream = std::cin.rdbuf(input_stream.rdbuf());
      char input[128];
      memclr(input,128,0);
      bool quit = false;
@@ -283,6 +257,7 @@ Error error;
          memclr(input, 128, 0);
          std::cin.clear();
      }
+*/
     return 0;
 }
 
