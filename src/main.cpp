e55da5ea276ce5b49623650b69cb1704873abe91
Jack.WSL <foxintango@yeah.net>
2023-06-12 00:48:02 +0800

    alpine

--- src/main.cpp
+++ src/main.cpp
@@ -459,7 +459,7 @@ Error error;
     Array<unsigned int> hash_array;
 
     //memclr(content,128,0);
-    for(unsigned int i = 0;i < 100000;i ++){
+    for(unsigned int i = 0;i < 50000;i ++){
         
         std::string s = std::to_string(i);
         //s += std::to_string(i * ~i);
@@ -468,13 +468,15 @@ Error error;
         //std::cout << "make hash -- " << i << " : " << hash << std::endl;
     }
 
+    std::cout << "make hash end." << std::endl;
+
     for(unsigned int i = 0;i < hash_array.size();i ++){
         for(unsigned int j = i + 1; j < hash_array.size(); j++){
             if(hash_array[i] == hash_array[j]){
                 std::cout << "哈希冲突: " << "index : j=" << j  << " i=" << i << " hash=" << hash_array[j] << std::endl;
             }
 
-            //else std::cout << "hash code : " << std::bitset<32>(hash_array[j]) << std::endl;
+            else std::cout << "hash code : " << std::bitset<32>(hash_array[j]) << std::endl;
         }
     }
     return 0;
