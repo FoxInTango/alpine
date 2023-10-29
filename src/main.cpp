a55ecda52a77d7ab983ef673737c98a5a81bdfa0
Jack.Win <foxintango@yeah.net>
2023-10-29 15:08:57 +0800

    alpine

--- src/main.cpp
+++ src/main.cpp
@@ -48,7 +48,7 @@ int main(int argc, char* argv[]) {
     printf("Error: %d,%s\n",e.code,e.description);
     //Model alpine_startup_model(1);
 
-    ME me;
+    ME me("anything matter?");
     ELFFile elf;
     int r = elf.open("lib/libcpp.so");
 
