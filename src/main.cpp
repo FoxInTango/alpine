c21611489a4a12766ff97aae947293500fe80b26
Jack.Win <foxintango@yeah.net>
2023-11-21 16:20:02 +0800

    alpine

--- src/main.cpp
+++ src/main.cpp
@@ -41,41 +41,15 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace foxintango;
+
+#include "../tst/tstcpp.h"
 int main(int argc, char* argv[]) {
     arguments startup_arguments(argc,argv);
 
     startup_arguments.echo();
     alpine.init(startup_arguments);
 
-    tree_node<int> n1,n2;
-
-    n1 = 2;
-    n2 = 3;
-
-    rb_tree_node<int> r1,r2;
-    r1 = 3;
-    r2 = 4;
-
-    if (n1 == n2) {
-        printf("n1 == n2.\n");
-    }
-    else { printf("n1 != n2.\n"); }
-
-    if (r1 == r2) {
-        printf("r1 == r2.\n");
-    }
-    else { printf("r1 != r2.\n"); }
-
-    r1.insert(20);
-
-    if(r1.m_left){
-        printf("r1 left subnode value %d\n", r1.m_left->t);
-    }
-
-    if (r1.m_right) {
-        printf("r1 right subnode value %d\n", r1.m_right->t);
-    }
-
+    libcpp_tst();
     return 0;
     switch(CurrentEndian()){
         case ENDIAN_B:{
