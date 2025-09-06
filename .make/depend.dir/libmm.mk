e5f843041b7659aa8d523b5175bcb9db1769c795
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-07 06:34:47 +0800

    Sun Sep  7 06:34:47 AM CST 2025

--- .make/depend.dir/libmm.mk
+++ .make/depend.dir/libmm.mk
@@ -1,4 +1,4 @@
-DEPEND_TARGETS  += libmm.build
+DEPEND_TARGETS  += libmm.recursive
 UPDATE_TARGETS  += libmm.update
 PUBLISH_TARGETS += libmm.publish
 ECHO_TARGETS    += libmm.echo
@@ -7,104 +7,10 @@ libmm:
 	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
 	cd /home/lidali/alpine/libraries/libmm/ && make && make install
 	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.build:
+libmm.recursive:
 	@echo SUPER_MAKE_DIR=/home/lidali/alpine/               >> /home/lidali/alpine/libraries/libmm/.make/super
 	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make && make install
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.clean:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make clean
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.prepare:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make prepare
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.install:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make install
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.uninstall:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make uninstall
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.publish:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make publish
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.update:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make update
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.echo:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make echo
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-DEPEND_TARGETS  += libmm.build
-ECHO_TARGETS    += libmm.echo
-libmm:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/               >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make && make install
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.build:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libmodel/               >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libmodel/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make && make install
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.clean:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make clean
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.prepare:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make prepare
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.install:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make install
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.uninstall:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make uninstall
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.publish:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make publish
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.update:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make update
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.echo:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make echo
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-DEPEND_TARGETS  += libmm.build
-ECHO_TARGETS    += libmm.echo
-libmm:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/               >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make && make install
-	-rm /home/lidali/alpine/libraries/libmm/.make/super
-libmm.build:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libmodel/               >> /home/lidali/alpine/libraries/libmm/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libmodel/.make >> /home/lidali/alpine/libraries/libmm/.make/super
-	cd /home/lidali/alpine/libraries/libmm/ && make && make install
+	cd /home/lidali/alpine/libraries/libmm/ && make recursive && make install
 	-rm /home/lidali/alpine/libraries/libmm/.make/super
 libmm.clean:
 	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/libraries/libmm/.make/super
---
+++ .make/depend.dir/mit.mk
@@ -0,0 +1,49 @@
+DEPEND_TARGETS  += mit.recursive
+UPDATE_TARGETS  += mit.update
+PUBLISH_TARGETS += mit.publish
+ECHO_TARGETS    += mit.echo
+mit:
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/               >> /home/lidali/alpine/applications/mit/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/applications/mit/.make/super
+	cd /home/lidali/alpine/applications/mit/ && make && make install
+	-rm /home/lidali/alpine/applications/mit/.make/super
+mit.recursive:
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/               >> /home/lidali/alpine/applications/mit/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/applications/mit/.make/super
+	cd /home/lidali/alpine/applications/mit/ && make recursive && make install
+	-rm /home/lidali/alpine/applications/mit/.make/super
+mit.clean:
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/applications/mit/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/applications/mit/.make/super
+	cd /home/lidali/alpine/applications/mit/ && make clean
+	-rm /home/lidali/alpine/applications/mit/.make/super
+mit.prepare:
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/applications/mit/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/applications/mit/.make/super
+	cd /home/lidali/alpine/applications/mit/ && make prepare
+	-rm /home/lidali/alpine/applications/mit/.make/super
+mit.install:
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/applications/mit/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/applications/mit/.make/super
+	cd /home/lidali/alpine/applications/mit/ && make install
+	-rm /home/lidali/alpine/applications/mit/.make/super
+mit.uninstall:
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/applications/mit/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/applications/mit/.make/super
+	cd /home/lidali/alpine/applications/mit/ && make uninstall
+	-rm /home/lidali/alpine/applications/mit/.make/super
+mit.publish:
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/applications/mit/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/applications/mit/.make/super
+	cd /home/lidali/alpine/applications/mit/ && make publish
+	-rm /home/lidali/alpine/applications/mit/.make/super
+mit.update:
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/applications/mit/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/applications/mit/.make/super
+	cd /home/lidali/alpine/applications/mit/ && make update
+	-rm /home/lidali/alpine/applications/mit/.make/super
+mit.echo:
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/          >> /home/lidali/alpine/applications/mit/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/applications/mit/.make/super
+	cd /home/lidali/alpine/applications/mit/ && make echo
+	-rm /home/lidali/alpine/applications/mit/.make/super
