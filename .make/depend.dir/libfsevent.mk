3852d6b3833f38c54d114b417b742da86beb9391
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-18 22:49:03 +0800

    Thu Sep 18 10:49:03 PM CST 2025

--- .make/depend.dir/libfsevent.mk
+++ .make/depend.dir/libfsevent.mk
@@ -3,47 +3,47 @@ UPDATE_TARGETS  += libfsevent.update
 PUBLISH_TARGETS += libfsevent.publish
 ECHO_TARGETS    += libfsevent.echo
 libfsevent:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/                >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make  >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	cd /home/lidali/alpine/libraries/libfsevent/ && make && make install
-	-rm /home/lidali/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/                >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/.make  >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libfsevent/ && make && make install
+	-rm /volumes/llama/home/alpine/libraries/libfsevent/.make/super
 libfsevent.recursive:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/               >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	cd /home/lidali/alpine/libraries/libfsevent/ && make recursive && make install
-	-rm /home/lidali/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/               >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/.make >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libfsevent/ && make recursive && make install
+	-rm /volumes/llama/home/alpine/libraries/libfsevent/.make/super
 libfsevent.clean:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/               >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	cd /home/lidali/alpine/libraries/libfsevent/ && make clean
-	-rm /home/lidali/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/               >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/.make >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libfsevent/ && make clean
+	-rm /volumes/llama/home/alpine/libraries/libfsevent/.make/super
 libfsevent.prepare:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/               >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	cd /home/lidali/alpine/libraries/libfsevent/ && make prepare
-	-rm /home/lidali/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/               >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/.make >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libfsevent/ && make prepare
+	-rm /volumes/llama/home/alpine/libraries/libfsevent/.make/super
 libfsevent.install:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/               >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	cd /home/lidali/alpine/libraries/libfsevent/ && make install
-	-rm /home/lidali/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/               >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/.make >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libfsevent/ && make install
+	-rm /volumes/llama/home/alpine/libraries/libfsevent/.make/super
 libfsevent.uninstall:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/               >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	cd /home/lidali/alpine/libraries/libfsevent/ && make uninstall
-	-rm /home/lidali/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/               >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/.make >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libfsevent/ && make uninstall
+	-rm /volumes/llama/home/alpine/libraries/libfsevent/.make/super
 libfsevent.publish:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/               >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	cd /home/lidali/alpine/libraries/libfsevent/ && make publish
-	-rm /home/lidali/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/               >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/.make >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libfsevent/ && make publish
+	-rm /volumes/llama/home/alpine/libraries/libfsevent/.make/super
 libfsevent.update:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/               >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	cd /home/lidali/alpine/libraries/libfsevent/ && make update
-	-rm /home/lidali/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/               >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/.make >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libfsevent/ && make update
+	-rm /volumes/llama/home/alpine/libraries/libfsevent/.make/super
 libfsevent.echo:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/               >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/.make >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	cd /home/lidali/alpine/libraries/libfsevent/ && make echo
-	-rm /home/lidali/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/               >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/.make >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libfsevent/ && make echo
+	-rm /volumes/llama/home/alpine/libraries/libfsevent/.make/super
